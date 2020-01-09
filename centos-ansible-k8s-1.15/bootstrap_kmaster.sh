1135a02b4812aa70f4261e77c55eeaede3b79056
shaun_zhou <uranus-neptune@hotmail.com>
2020-01-10 00:08:49 +0800

    [Update] set k8s v1.15.0

--- centos-ansible-k8s-1.15/bootstrap_kmaster.sh
+++ centos-ansible-k8s-1.15/bootstrap_kmaster.sh
@@ -2,7 +2,7 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers --apiserver-advertise-address=172.17.8.101 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --kubernetes-version=v1.15.0 --image-repository=registry.cn-hangzhou.aliyuncs.com/google_containers --apiserver-advertise-address=172.17.8.101 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
